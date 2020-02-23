1fe28ff6efa00655ac3f6d53abb2a180bbd6b2d0
shinegojoe <shinegojoe@gmail.com>
2020-02-23 20:46:09 +0800

    template_generactor change

--- python_scripts/test_file_template_generator.py
+++ python_scripts/test_file_template_generator.py
@@ -2,28 +2,32 @@ import os
 
 
 class TestFileTemplateGenerator:
-    def __init__(self):
+    def __init__(self, input_folder, output_folder):
+        self.input_folder = input_folder
+        self.output_folder = output_folder
         self.string_filter_list = ['for', 'if', 'else', 'class', 'constructor', 'try', 'catch']
-        self.output_folder = 'output'
 
     def _write_import(self, model_name):
         with open(self.output_path, 'a') as f:
             line = "import {{}} from '@/models/folder_name/{}.js'".format(model_name, model_name)
-            f.write(line)
+            line2 = "const assert = require('chai').assert"
+            f.write(line + '\n')
+            f.write(line2 + '\n')
             f.write('\n')
             f.write('\n')
 
-    def _load_files(self, folder_path):
-        files = os.listdir(folder_path)
+    def _load_files(self):
+        files = os.listdir(self.input_folder)
         for file_path in files:
             print(file_path)
-            path = '{}/{}'.format(folder_path, file_path)
+            path = '{}/{}'.format(self.input_folder, file_path)
             output_name = file_path.replace('model', 'model_test')
             model_name = file_path.rstrip('.js')
             self.output_path = '{}/{}'.format(self.output_folder, output_name)
 
             self._write_import(model_name=model_name)
-            self._write_file(self.output_path, model_name)
+            # self._write_file(self.output_path, model_name)
+            self._write_first_descrbe(model_name)
             with open(path, 'r') as f:
                 data = f.readlines()
                 self._parse_property(data=data)
@@ -34,31 +38,26 @@ class TestFileTemplateGenerator:
                 return False
         return True
 
+    def _write_first_descrbe(self, name):
+        with open(self.output_path, 'a') as f:
+            # line = "describe('' test', function(){".format('')
+            line = "describe('{} test', function()){{".format(name)
+            f.write(line + '\n')
+
+
     def _write_file(self, file_path, property):
-        if not os.path.exists(file_path):
-            with open(file_path, 'w') as f:
-                # line = "describe('' test', function(){".format('')
-                line = "describe('{} test', function()){{".format(property)
-                f.write(line + '\n')
-        else:
-            with open(file_path, 'a') as f:
-                if property == '})':
-                    f.write(property)
-                else:
-                    line1 = "   describe('{}', function(){{".format(property)
-                    f.write(line1 + '\n')
-                    line2 = "       it('', function(){"
-                    line3 = "       })"
-                    line4 = "   })"
-                    f.write(line2 + '\n')
-                    f.write(line3 + '\n')
-                    f.write(line4 + '\n')
-
-
-
-
-
-
+        with open(file_path, 'a') as f:
+            if property == '})':
+                f.write(property)
+            else:
+                line1 = "   describe('{}', function(){{".format(property)
+                f.write(line1 + '\n')
+                line2 = "       it('', function(){"
+                line3 = "       })"
+                line4 = "   })"
+                f.write(line2 + '\n')
+                f.write(line3 + '\n')
+                f.write(line4 + '\n')
 
     def _parse_property(self, data):
         for line in data:
@@ -70,13 +69,17 @@ class TestFileTemplateGenerator:
                     line = line.strip()
                     print(line)
                     self._write_file(self.output_path, line)
+    def run(self):
+        self._load_files()
+
 
 
 
 def main():
-    folder = 'test_folder'
-    file_generator = TestFileTemplateGenerator()
-    file_generator._load_files(folder_path=folder)
+    input_folder = 'test_folder'
+    output_folder = 'output'
+    file_generator = TestFileTemplateGenerator(input_folder=input_folder, output_folder=output_folder)
+    file_generator.run()
 
 
 
